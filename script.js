844d920b3abbd63367a2c1f6ea598ffcb48a36a1
alina05sergeevna-debug <alina05sergeevna@mail.ru>
2026-02-19 11:27:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById('start-box').addEventListener('click', function() {
     // Ссылка на оплату
-    const paymentLink = "https://payform.ru/plaIrAy/";
+    const paymentLink = "https://payform.ru/suaIs8y/";
 
     // Простой редирект на страницу оплаты
     window.location.href = paymentLink;
 });
+
 
 
 
