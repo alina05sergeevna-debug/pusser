219769cbcbe87ea1900ac74d22043d4a11bb907e
alina05sergeevna-debug <alina05sergeevna@mail.ru>
2026-02-19 07:54:11 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.getElementById('start-box').addEventListener('click', function() {
-    window.location.href = "https://markprodamus.link.sendsay.ru/markprodamus/8000,=0AHnUPebI4Nx2kympCrHPBw/8930,2543389,140017,?aHR0cHM6Ly9wdXNzZXJyLnBheWZvcm0ucnU=";
+    window.location.href = "https://payform.ru/72aI9JT/";
 });
+
