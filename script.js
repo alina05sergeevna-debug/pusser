8ed555ae3b7083c1d02000d4965a9b7499b74755
alina05sergeevna-debug <alina05sergeevna@mail.ru>
2026-02-19 08:34:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.getElementById('start-box').addEventListener('click', function() {
-    window.location.href = "https://payform.ru/ogaIpeM/";
+document.querySelector('button').addEventListener('click', function() {
+    // Запуск формы оплаты при клике на кнопку
+    const price = this.getAttribute('data-prodamusprice');
+    const currency = this.getAttribute('data-currency');
+
+    // Открытие формы оплаты с переданными данными
+    window.location.href = `https://payform.ru/${price}?currency=${currency}`;
 });
+
+
 
 
